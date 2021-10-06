db01efce48ed8c088fe637ef659cbb32bfeaffe7
JJambbong <91708960+JJambbong@users.noreply.github.com>
2021-10-06 20:56:17 +0900

    Add files via upload

---
+++ DiscordBot.py
@@ -0,0 +1,23 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 16
+VisualStudioVersion = 16.0.31624.102
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{888888A0-9F3D-457C-B088-3A5042F75D52}") = "Discord Bot", "Discord Bot\Discord Bot.pyproj", "{914EE120-2DEE-49EA-AC9E-9FA22F666C52}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|Any CPU = Debug|Any CPU
+		Release|Any CPU = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{914EE120-2DEE-49EA-AC9E-9FA22F666C52}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+		{914EE120-2DEE-49EA-AC9E-9FA22F666C52}.Release|Any CPU.ActiveCfg = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {1F296B08-A9DB-493A-A1A2-9CB001E13151}
+	EndGlobalSection
+EndGlobal
